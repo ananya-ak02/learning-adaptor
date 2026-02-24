29adeecd3ca12d154f970e1567eec359b3558d0b
Ananya Khanduja <akhanduja_be24@thaapr.edu>
2026-02-24 19:13:29 +0530

    Fix invalid next.config.ts options

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   typescript: {
     // This allows production builds to successfully complete even if
     // your project has TypeScript errors.
@@ -12,4 +8,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
